cdb319c71ce8a4bb6cef3c35b62356002c7157bd
abhijith2021 <88226109+abhijith2021@users.noreply.github.com>
2024-11-29 19:06:56 +0530

    4th commit

--- login.js
+++ login.js
@@ -1,1 +1,15 @@
-const name = document.getElementById('fname').innerHTML = 5
+
+function show(){
+var fname = document.getElementById('fname').value;
+// var name_main = fname.value;
+var phn = document.getElementById('phn').value;
+var pass = document.getElementById('pass').value;
+var cnf = document.getElementById('cnf').value;
+var mail = document.getElementById('mail').value;
+console.log(fname);
+// console.log(mail);
+console.log(phn);
+console.log(pass);
+console.log(cnf);
+console.log(mail);
+}
