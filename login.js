e72d94c14db9c3704a43d7aafffddbc805bb74be
abhijith2021 <Abhijithkishoretvm20@gmail.com>
2024-12-01 14:44:22 +0530

    learnings

--- login.js
+++ login.js
@@ -1,15 +1,38 @@
 
-function show(){
-var fname = document.getElementById('fname').value;
-// var name_main = fname.value;
-var phn = document.getElementById('phn').value;
-var pass = document.getElementById('pass').value;
-var cnf = document.getElementById('cnf').value;
-var mail = document.getElementById('mail').value;
-console.log(fname);
-// console.log(mail);
-console.log(phn);
-console.log(pass);
-console.log(cnf);
-console.log(mail);
+// function show(){
+// var fname = document.getElementById('fname').value;
+// // var name_main = fname.value;
+// var phn = document.getElementById('phn').value;
+// var pass = document.getElementById('pass').value;
+// var cnf = document.getElementById('cnf').value;
+// var mail = document.getElementById('mail').value;
+// console.log("your first name is:" + fname);
+// // console.log(mail);
+// console.log("your phone number is:" + phn);
+// console.log("your password is:" + pass);
+// console.log(cnf);
+// console.log("your mail id is:" + mail);
+// const a = [];
+// console.log(a.push({name: fname, password: pass}))
+// console.log(a)
+
+// }
+
+function validate(fname){
+    
+    console.log(fname);
+    var namePattern = /^[a-zA-z]$/;
+    if (!namePattern.test(fname)){
+        alert( "invalid name entered");
+    }
+    alert("you are good to go");
+    
+    
+}
+function validForm(){
+    var firstname = document.getElementById("fname").value;
+    
+    var validNotification = validate(firstname);
+    // ocument.getElementById("notification").textContent = validNotification;
+    console.log(validNotification);
 }
