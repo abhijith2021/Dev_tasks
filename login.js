50a8c8a57a61d42d5d1fb6e521e9998597736746
abhijith2021 <Abhijithkishoretvm20@gmail.com>
2024-12-04 00:01:17 +0530

    commit

--- login.js
+++ login.js
@@ -19,38 +19,75 @@
 // }
 
 function validate(fname,mail)
+
 {
     
     console.log(fname);
-    var namePattern = /^[a-zA-z\s]$/;
-    if (!namePattern.test(fname))
+    var mailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    var namePattern = /^[a-zA-Z\s]+$/;
+    if (namePattern.test(fname) && mailpattern.test(mail))
         {
-         alert( "invalid name entered");
-         return "invalid name";
+         alert( "data is valid");
+         return "successfull";
+         
+        }
+        else{
+            if(!namePattern.test(fname) && !mailpattern.test(mail)){
+                alert("data is invalid");
+                return "retype again";
+            }
         }
     
-    var mailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/;
-    if(!mailpattern.test(mail)){
-        alert("invalid mail entered");
-        return "invalid mail";
+    // var mailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    // if(!mailpattern.test(mail)){
+    //     alert("invalid mail entered");
+    //     return "invalid mail";
 
-    }
+    // }
     
-    alert("you are good to go");
-    return "valid";
+    
+    
+//     return "valid";
+// }
+// function validForm(){
+//     var firstname = document.getElementById("fname").value;
+//     var emailAddress = document.getElementById("mail").value
+//     var validNotification = validate(firstname,emailAddress);
+//     // var mailNotification = validate(emailAddress);
+//     if (validNotification === "valid"){
+//         document.getElementById("notification").textContent = "form submitted successfully";
+//     }
+//     else{
+//         document.getElementById("notification").textContent = "invalid credentials entered"
+//     }
+    
+//     console.log(validNotification);
+//     // console.log(mailNotification);
+
+//     // code to return entries
+//     var fname = document.getElementById('fname').value;
+// // var name_main = fname.value;
+//     var phn = document.getElementById('phn').value;
+//     var pass = document.getElementById('pass').value;
+//     var cnf = document.getElementById('cnf').value;
+//     var mail = document.getElementById('mail').value;
+//     console.log("your first name is:" + fname);
+//     // console.log(mail);
+//     console.log("your phone number is:" + phn);
+//     console.log("your password is:" + pass);
+//     console.log(cnf);
+//     console.log("your mail id is:" + mail);
+//     const a = [];
+//     console.log(a.push({name: fname, password: pass}))
+//     console.log(a)
 }
-function validForm(){
-    var firstname = document.getElementById("fname").value;
-    var emailAddress = document.getElementById("mail").value
-    var validNotification = validate(firstname);
-    var mailNotification = validate(emailAddress);
-    if (validNotification === "valid" && mailNotification === "valid"){
-        document.getElementById("notification").textContent = "form submitted successfully";
+
+function toggleVisible(){
+    var pass = document.getElementById("pass").ariaValueMax;
+    if(pass.type === "pswrd"){
+        pass.type = "text";
     }
     else{
-        document.getElementById("notification").textContent = "invalid credentials entered"
+        pass.type = "password";
     }
-    
-    console.log(validNotification);
-    console.log(mailNotification);
 }
